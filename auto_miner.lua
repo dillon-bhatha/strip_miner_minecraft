e9164d2a3ef12ea01a29ca2b35a4db00ba1c34aa
Dilmohit Singh Bhatha <145561841+dillon-bhatha@users.noreply.github.com>
2024-05-16 15:04:05 +0200

    Update auto_miner.lua

--- auto_miner.lua
+++ auto_miner.lua
@@ -1,27 +1,19 @@
 local function refuelIfNeeded()
     if turtle.getFuelLevel() == 0 then
-        local refueled = false
         for slot = 1, 16 do
             turtle.select(slot)
             if turtle.refuel(1) then
-                print("Bijgevuld!")
-                refueled = true
-                break
+                return true
             end
         end
-        if not refueled then
-            print("Geen brandstof meer")
-            return false
-        end
     end
-    return true
 end
 
 local function placeTorches()
     for _ = 1, 2 do
         turtle.turnRight()
     end
-    turtle.select(2)
+    turtle.select(2)  -- Selecteer het tweede slot (aangenomen dat dit het slot is waar de fakkels zich bevinden)
     turtle.place()
     for _ = 1, 2 do
         turtle.turnRight()
@@ -30,9 +22,7 @@ end
 
 local function stripMineAndPlaceTorches()
     for i = 1, 50 do
-        if not refuelIfNeeded() then
-            return
-        end
+        refuelIfNeeded()
         turtle.digUp()
         turtle.dig()
         turtle.forward()
