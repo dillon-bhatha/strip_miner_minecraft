d6e53a2684a54d93d9086c72e15ed5a3416a5897
Dilmohit Singh Bhatha <145561841+dillon-bhatha@users.noreply.github.com>
2024-05-16 15:06:22 +0200

    Update install.lua

--- install.lua
+++ install.lua
@@ -3,9 +3,13 @@ response = http.get("https://raw.githubusercontent.com/dillon-bhatha/strip_miner
 if response then
     local content = response.readAll()
     response.close()
-    local file = fs.open("chest_monitor.lua", "w")
+    local file = fs.open("auto_miner.lua", "w")
     file.write(content)
     file.close()
+
+    local startupFile = fs.open("startup.lua", "w")
+    startupFile.write("shell.run('auto_miner.lua')")
+    startupFile.close()
 
     shell.run("reboot")
 else
